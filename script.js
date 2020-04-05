4970ec18655bec114722e01c0fd0c9b5f9f884c5
Danielle Beaty <daniellerbeaty@gmail.com>
2020-04-05 02:49:29 -0400

    Homework (v3.0)

--- script.js
+++ script.js
@@ -6,7 +6,32 @@ $(window).on("load", function () {
 
 })
 // set global variables 
+var scheduleInput = document.querySelector("#textarea");
+var saveButton = document.querySelector("#saveText")
+
 
 // fuction to store input to local storage 
+
+$('#saveTextBtn').on('click' , function(){
+    event.preventDefault();
+    $('input[type="text"]').each(function(){
+        var id = $(this).attr('id');
+        var value = $(this).val();
+       localStorage.setItem(id, value);
+       return textarea;
+       
+});
+});
+
+
+// Retain Data in form when browser is refreshed 
+// function renderTextArea (){
+// var dataSaved = localStorage.getItem("#textarea")
+// if (dataSaved){
+//     return;
+// } 
+// }
+   
+
 // fuction to excute timeblock color coded to indicate whether it is in the past, present, or future
 
